f1116a3fb94d6b9483943a714287980c76452a61
Kim Nguyen <nnhk23@gmail.com>
2021-01-09 16:43:20 -0800

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -3,6 +3,12 @@ export default function manageTodo(state = {
 }, action) {
 
   console.log("reducer received this action:", action);
+  switch(action.type){
+    case 'ADD_TODO':
+      console.log({ todos: state.todos.concat(action.payload.text) });
 
-  return state;
+      return { todos: state.todos.concat(action.payload.text) };
+    default: 
+    return state
+  }
 }
